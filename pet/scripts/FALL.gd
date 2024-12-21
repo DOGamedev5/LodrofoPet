233f6bf435c72b72cf873be77e0c6bbd2518bcf8
Misael Victor <ribeiromisa05@gmail.com>
2024-12-21 18:11:59 -0300

    fix bug

--- pet/scripts/FALL.gd
+++ pet/scripts/FALL.gd
@@ -6,26 +6,26 @@ func enter(_ls):
 	if not parent.running:
 		$"../../AnimationPlayer".play("fall")
 
-func process_physics(_delta):
-	if OS.window_position.x < 0:
-		if parent.motion.x < 0:
-			parent.motion.x *= -parent.bounce
-			
-		parent.window_position_delta.x = 0
-		parent.motion.y /= 2
-		
-	if OS.window_position.x + OS.window_size.x > OS.get_screen_size().x:
-		if parent.motion.x > 0:
-			parent.motion.x *= -parent.bounce
-			
-		parent.window_position_delta.x = OS.get_screen_size().x - OS.window_size.x
-		parent.motion.y /= 2
-	
-	if OS.window_position.y < 0:
-		parent.motion.y = 0
-		parent.window_position_delta.y = 0
-		
-		parent.motion.x /= 2
+#func process_physics(_delta):
+#	if OS.window_position.x < 0:
+#		if parent.motion.x < 0:
+#			parent.motion.x *= -parent.bounce
+#
+#		parent.window_position_delta.x = 0
+#		parent.motion.y /= 2
+#
+#	if OS.window_position.x + OS.window_size.x > OS.get_screen_size().x:
+#		if parent.motion.x > 0:
+#			parent.motion.x *= -parent.bounce
+#
+#		parent.window_position_delta.x = OS.get_screen_size().x - OS.window_size.x
+#		parent.motion.y /= 2
+#
+#	if OS.window_position.y < 0:
+#		parent.motion.y = 0
+#		parent.window_position_delta.y = 0
+#
+#		parent.motion.x /= 2
 	
 func process_state():
 	if parent.onFloor():
