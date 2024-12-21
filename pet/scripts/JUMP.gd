d89df933e33cfb4816c5d3d3fda16c1c58c5129e
Misael Victor <ribeiromisa05@gmail.com>
2024-12-21 18:17:57 -0300

    fix bug from LodrofoPet

--- pet/scripts/JUMP.gd
+++ pet/scripts/JUMP.gd
@@ -2,6 +2,7 @@ extends State
 
 
 func enter(_ls):
+	parent.onFloor = false
 	parent.direction.x = 1 - ((randi() % 2) * 2)
 	
 	var movement : Vector2 = Vector2.DOWN * parent.jump_force * rand_range(0.3, 1.2)
